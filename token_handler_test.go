3d7bfe6e38da57d39acaef6148385eea127e9af4
hawry <hawry@hawry.net>
2018-01-05 18:53:09 +0100

    add tests for token handler

--- token_handler_test.go
+++ token_handler_test.go
@@ -3,6 +3,7 @@ package middlewares
 import (
 	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -33,5 +34,41 @@ func TestExtractBearer(t *testing.T) {
 		assert.NotNil(t, err, "should not be nil")
 		assert.Empty(t, tval, "should be empty")
 		assert.Equal(t, "no authentication header found", err.Error(), "should be equal")
+	})
+}
+
+func TestTokenHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/tokens", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("Empty token", func(t *testing.T) {
+		req.Header.Set("Authentication", "Bearer")
+
+		ctxHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			token, err := Token(r.Context())
+			assert.NotNil(t, err, "should not be nil")
+			assert.Empty(t, token, "should be empty")
+			assert.Equal(t, "no token found in context", err.Error(), "should be equal")
+		})
+
+		rr := httptest.NewRecorder()
+		handler := TokenHandler(ctxHandler)
+		handler.ServeHTTP(rr, req)
+	})
+
+	t.Run("With token", func(t *testing.T) {
+		req.Header.Set("Authentication", "Bearer thisisatoken")
+
+		ctxHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			token, err := Token(r.Context())
+			assert.Nil(t, err, "should be nil")
+			assert.Equal(t, "thisisatoken", token, "should be equal")
+		})
+
+		rr := httptest.NewRecorder()
+		handler := TokenHandler(ctxHandler)
+		handler.ServeHTTP(rr, req)
 	})
 }
