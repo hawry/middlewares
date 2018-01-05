c9c6936750f986b484805061e0fe317a344dfa7b
hawry <hawry@hawry.net>
2018-01-05 18:36:36 +0100

    move common variables and methods to a more obvious place

--- middlewares.go
+++ middlewares.go
@@ -1,10 +1,26 @@
 // Package middlewares aims to create a set of commonly used middleware http.Handlers for use with the default http package. All handlers only takes a http.Handler as an argument, and returns only http.Handler, to more easily be chained with handler chain libraries (e.g. https://github.com/justinas/alice)
 package middlewares
 
+import (
+	"io"
+	"os"
+)
+
 type contextKey string
 
+var output io.Writer
+
 const (
-	jwtContextKey  contextKey = "mw_jwt_context_key"
-	fpContextKey   contextKey = "mw_fp_context_key"
-	authContextKey contextKey = "mw_auth_context_key"
+	tokenContextKey contextKey = "mw_token_context_key"
+	fpContextKey    contextKey = "mw_fp_context_key"
+	authContextKey  contextKey = "mw_auth_context_key"
 )
+
+func init() {
+	output = os.Stdout
+}
+
+//SetOutput sets which io.Writer to print the log to. Default is os.Stdout.
+func SetOutput(w io.Writer) {
+	output = w
+}
