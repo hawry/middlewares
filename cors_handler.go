1fe89a98d1cf8b42661a361a0960398c6198fc4e
hawry <hawry@hawry.net>
2018-01-05 20:10:49 +0100

    begin work on cors handler

--- cors_handler.go
+++ cors_handler.go
@@ -1,1 +1,11 @@
 package middlewares
+
+var (
+	possibleCORSMethods = []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"}
+	allowedCORSMethods  = []string{}
+)
+
+//AllowCORSMethods specified which methods that are allowed for CORS
+func AllowCORSMethods(methods ...string) {
+	allowedCORSMethods = append(allowedCORSMethods, methods...)
+}
